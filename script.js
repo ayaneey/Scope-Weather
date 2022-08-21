3fdc797666dba427a360bf877f989294558cc761
ayaneey <ayan.h18@hotmail.com>
2022-08-21 20:19:27 +0100

    implementing current weather and time

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let APIKEY = "21d2cad1715d85751c0dddc41d01b75c";
 let cityName = document.getElementById("cityName");
 let weatherTime = document.getElementById("weatherTime");
 let weatherDay = document.getElementById("weatherDay");
-console.log(cityName);
+let tempValue = document.getElementById("tempValue");
 
 navigator.geolocation.getCurrentPosition((position) => {
   const { latitude, longitude } = position.coords;
@@ -34,12 +34,30 @@ function showWeather(data){
     let time = new Date(data.dt*1000)
     let hours = time.getHours();
     let minutes = "0" + time.getMinutes();
-    let seconds = "0" + time.getSeconds();
-    let formattedTime = hours + ':' + minutes.substring(-2) + ':' + seconds.substring(-2);
-    let day = time.toDateString();
-    console.log(day);
-    
+    let formattedTime = `${hours}:${minutes}`;
+    let day = time.toDateString().split(" ")[0];
     weatherTime.innerHTML = formattedTime;
+    weatherDay.innerHTML = day;
+    cityName.innerHTML = data.name;
+    tempValue.innerHTML = (data.main.temp - 292).toFixed(0);
+
+    //showing the weather icon based on current weather information 
+    if(data.weather[0].main == "Clear") {
+      weatherIcon.src = "./icons/sun.svg";
+    } else if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "./icons/clouds.svg";
+    } else if (data.weather[0].main == "Cloud") {
+      weatherIcon.src = "./icons/cloud.svg";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "./icons/rain.svg";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "./icons/snow.svg";
+    } else if (data.weather[0].main == "Thunderstorm") {
+      weatherIcon.src = "./icons/thunderstorm.svg";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "./icons/clouds-and-sun.svg";
+    }
+
 }
 
 
