a6f43ceeafe52b4a81a6c6a24d5d2d3859be604f
ayaneey <ayan.h18@hotmail.com>
2022-08-22 10:06:24 +0100

    amended weather values container to show accurate representation of weather humidity, pressure and wind

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let cityName = document.getElementById("cityName");
 let weatherTime = document.getElementById("weatherTime");
 let weatherDay = document.getElementById("weatherDay");
 let tempValue = document.getElementById("tempValue");
+var weatherIcon = "./icons/sun.svg";
+var weatherIcon = "./icons/clouds.svg";
+var weatherIcon = "./icons/cloud.svg";
+var weatherIcon = "./icons/rain.svg";
+var weatherIcon = "./icons/snow.svg";
+var weatherIcon = "./icons/thunderstorm.svg";
+var weatherIcon = "./icons/clouds-and-sun.svg";
+let weatherPressure = document.getElementById("weatherPressure");
+let weatherHumidity = document.getElementById("weatherHumidity");
+let weatherWind = document.getElementById("weatherWind");
+
+
 
 navigator.geolocation.getCurrentPosition((position) => {
   const { latitude, longitude } = position.coords;
@@ -21,46 +33,50 @@ function getWeather(latitude, longitude) {
     .then((response) => {
       return response.json();
     })
-    .then((data) => 
-    {
-        showWeather(data);
-    })
+    .then((data) => {
+      showWeather(data);
+    });
 }
 
-function showWeather(data){
-    console.log(data);
+function showWeather(data) {
+  console.log(data);
 
-    //getting the time for the weather
-    let time = new Date(data.dt*1000)
-    let hours = time.getHours();
-    let minutes = "0" + time.getMinutes();
-    let formattedTime = `${hours}:${minutes}`;
-    let day = time.toDateString().split(" ")[0];
-    weatherTime.innerHTML = formattedTime;
-    weatherDay.innerHTML = day;
-    cityName.innerHTML = data.name;
-    tempValue.innerHTML = (data.main.temp - 292).toFixed(0);
+  //getting the time for the weather
+  let time = new Date(data.dt * 1000);
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let formattedTime = `${hours}:${minutes}`;
+  let day = time.toDateString().split(" ")[0];
+  weatherTime.innerHTML = formattedTime;
+  weatherDay.innerHTML = day;
+  cityName.innerHTML = data.name;
+  tempValue.innerHTML = (data.main.temp - 273.15).toFixed(0);
 
-    //showing the weather icon based on current weather information 
-    if(data.weather[0].main == "Clear") {
-      weatherIcon.src = "./icons/sun.svg";
-    } else if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "./icons/clouds.svg";
-    } else if (data.weather[0].main == "Cloud") {
-      weatherIcon.src = "./icons/cloud.svg";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "./icons/rain.svg";
-    } else if (data.weather[0].main == "Snow") {
-      weatherIcon.src = "./icons/snow.svg";
-    } else if (data.weather[0].main == "Thunderstorm") {
-      weatherIcon.src = "./icons/thunderstorm.svg";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "./icons/clouds-and-sun.svg";
-    }
+  //showing the weather icon based on current weather information
+  if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "./icons/sun.svg";
+  } else if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "./icons/clouds.svg";
+  } else if (data.weather[0].main == "Cloud") {
+    weatherIcon.src = "./icons/cloud.svg";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "./icons/rain.svg";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "./icons/snow.svg";
+  } else if (data.weather[0].main == "Thunderstorm") {
+    weatherIcon.src = "./icons/thunderstorm.svg";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "./icons/clouds-and-sun.svg";
+  }
+
+weatherPressure.innerHTML = data.main.pressure;
+weatherHumidity.innerHTML = data.main.humidity;
+weatherWind.innerHTML = data.wind.speed;
 
 }
 
 
 
+// showing upcoming weather based on weather information provided
 
 
